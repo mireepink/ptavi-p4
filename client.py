1def9f99354279bb72e95216648ee0471c78adf6
alvaromv83 <alvaromv83@gmail.com>
2014-10-20 12:22:13 +0200

    Fin Ej. 6 - Modificado client.py para que envíe registro SIP

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Programa cliente que abre un socket a un servidor
+Programa cliente que abre un socket a un servidor y envía mensajes SIP
 """
 
 import socket
@@ -13,20 +13,22 @@ import sys
 try:
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
-    LINE = sys.argv[3]
+    METHOD = sys.argv[3].upper()
+    USER = sys.argv[4]
 except IndexError:
-    print ("Usage: python client.py ip port msg")
+    print ("Usage: python client.py ip server_port SIP_method user")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
+request = METHOD + " " + "sip:" + USER + " SIP/1.0" + '\r\n\r\n'
+print "Enviando:\n" + request
+my_socket.send(request)
 data = my_socket.recv(1024)
 
-print 'Recibido -- ', data
+print data
 print "Terminando socket..."
 
 # Cerramos todo
