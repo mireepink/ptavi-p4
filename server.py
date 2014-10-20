7181f2ce45057d040d97534020da0acbb19384c0
alvaromv83 <alvaromv83@gmail.com>
2014-10-20 12:22:57 +0200

    Fin Ej. 6 - Modificado server.py para que sea Registrar SIP

--- server.py
+++ server.py
@@ -2,27 +2,39 @@
 # -*- coding: iso-8859-15 -*-
 """
 Clase (y programa principal) para un servidor de eco
-en UDP simple
+en UDP simple. Registra los clientes SIP.
 """
 
 import SocketServer
 import sys
 
 
-class EchoHandler(SocketServer.DatagramRequestHandler):
+class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
-        print "IP del cliente: " + str(self.client_address[0]),
-        print "| Puerto del cliente: " + str(self.client_address[1])
+        client_ip = str(self.client_address[0])
+        client_port = str(self.client_address[1])
+        print "IP del cliente: " + client_ip,
+        print "| Puerto del cliente: " + client_port
+        client_dic = {}
+
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print "El cliente nos manda " + line
+            param_list = line.split(" ")
+            param_list[-1] = param_list[-1].split('\r')[0]
+            if param_list != [""]:
+                method = param_list[0]
+                user = param_list[1]
+                version = param_list[2]
+
+                if method == 'REGISTER':
+                    client_dic[user] = client_ip
+                    self.wfile.write(version + " 200 OK\r\n\r\n")
             if not line:
                 break
 
@@ -33,6 +45,6 @@ if __name__ == "__main__":
     except IndexError:
         print ("Usage: python server.py server_port")
 
-    serv = SocketServer.UDPServer(("", port), EchoHandler)
+    serv = SocketServer.UDPServer(("", port), SIPRegisterHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
