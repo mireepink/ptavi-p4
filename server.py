5a72630bb94ca10b324c54810a78f4990f73836c
silviamaa <silvia_cok92@hotmail.com>
2014-10-25 12:58:36 +0200

    server dicc ejercicio7

--- server.py
+++ server.py
@@ -9,21 +9,36 @@ import sys
 
 puerto = sys.argv[1]
 
+dicc_usuario = {}
+
 fichero = open('fichero.txt', 'w')
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Registro SIP
     """
+
     def handle(self):
-		ip = str(self.client_address[0])
-		puerto = str(self.client_address[1])
-		self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
-		while 1:
-			line = self.rfile.read()
-			line1 = line.split(' ')
-			if (line1[0] == 'REGISTER'):
-				fichero.write(ip + " " + puerto)
+        ip = str(self.client_address[0])
+        puerto = str(self.client_address[1])
+        self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+        while 1:
+            line = self.rfile.read()
+            line1 = line.split()
+            if (line1[0] == 'REGISTER'):
+                fichero.write(ip + " " + puerto)
+            line2 = line1[1]
+            line3 = line2.split(":")
+			#añado un valor a la clave del dicc_usuario
+            dicc_usuario[line3[1]] = ip
+            print dicc_usuario
+            if line1[4] == '0':
+                busca_clave = dicc_usuario.has_key(line3[1])
+                if busca_clave == 1:
+                    del dicc_usuario[line3[1]]
+                    print dicc_usuario
+                    self.wfile.write("El usuario se ha borrado\r\n")
+                    self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
