eef2a197ae6932710f7c7b873275f594694da71a
mireya elisabet valenzuela carballo <mireya@zeta13.aulas.gsyc.es>
2014-10-27 14:29:49 +0100

    ej 7 expires server

--- server.py
+++ server.py
@@ -18,12 +18,18 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             if line_list[0] == 'REGISTER':
 		direccion = line_list[1].split(':')[1]
                 diccionario[direccion] =  self.client_address[0]
-                print "Enviando", "SIP/2.0 200 OK" + '\r\n\r\n' 
+                print "Enviando ", "SIP/2.0 200 OK" + " REGISTER" + '\r\n\r\n' 
+                self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
+                expires = int(line_list[4])
+                print diccionario
+                if expires == 0:
+                    del diccionario[direccion]
+                print diccionario
+                print "Enviando ", "SIP/2.0 200 OK" + " EXPIRES" + '\r\n\r\n' 
                 self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
             if not line:
                 break
         
-
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     diccionario = {}
