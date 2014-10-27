b4f131e5d6756ead035db18d0f272cf5947d6f40
javier baos mora <jbaos@iota27.aulas.gsyc.es>
2014-10-27 12:48:31 +0100

    cliente modificado ejercicio 8

--- client.py
+++ client.py
@@ -7,29 +7,28 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 
-# Cliente UDP simple.
-
-# Dirección IP del servidor.
 Entrada = sys.argv
+
+if len(Entrada) != 6:
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
+
 IP = Entrada[1]
 PORT = int(Entrada[2])
-PETICION = Entrada[3]
+PETICION = Entrada[3].upper()
 
-# Contenido que vamos a enviar
 DIRECCION = Entrada[4]
+TIEMPO = Entrada[5]
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((IP, PORT))
 
-print "Enviando: " + PETICION + " sip: " + DIRECCION
-my_socket.send(PETICION + " sip: " + DIRECCION + ' SIP/1.0 ' + '\r\n\r\n')
-data = my_socket.recv(1024)
-
+print PETICION + " sip:" + DIRECCION
+print "Expires: " + TIEMPO
+my_socket.send(PETICION + " sip: " + DIRECCION + ' SIP/2.0 ' + '\r\n' + "Expires: " + TIEMPO + '\r\n\r\n')
+data = my_socket.recv(1024)    
 print 'Recibido -- ', data
 print "Terminando socket..."
 
-# Cerramos todo
 my_socket.close()
 print "Fin."
