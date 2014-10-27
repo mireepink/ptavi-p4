f695f4c95220d1025e1ba6901bf876bbd08071c7
mireya elisabet valenzuela carballo <mireya@iota13.aulas.gsyc.es>
2014-10-27 16:42:03 +0100

    ej 8 + pep8 server.py

--- server.py
+++ server.py
@@ -1,38 +1,49 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
-"""
-Clase (y programa principal) para un servidor de eco
-en UDP simple
-"""
+
 import sys
 import SocketServer
+import time
 
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
-    
+
     def handle(self):
-        print self.client_address 
         while 1:
-	    line = self.rfile.read()
-            line_list = line.split(' ')
-            if line_list[0] == 'REGISTER':
-		direccion = line_list[1].split(':')[1]
-                diccionario[direccion] =  self.client_address[0]
-                print "Enviando ", "SIP/2.0 200 OK" + " REGISTER" + '\r\n\r\n' 
-                self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
-                expires = int(line_list[4])
-                print diccionario
-                if expires == 0:
-                    del diccionario[direccion]
-                print diccionario
-                print "Enviando ", "SIP/2.0 200 OK" + " EXPIRES" + '\r\n\r\n' 
-                self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
+            line = self.rfile.read()
             if not line:
                 break
-        
+            line_list = line.split(' ')
+            IP = self.client_address[0]
+            if line_list[0] == 'REGISTER':
+                direccion = line_list[1].split(':')[1]
+                expires = int(line_list[4])
+                timenow = time.time()
+                timexp = timenow + expires
+                valores = [IP, timexp]
+                diccionario[direccion] = valores
+                print diccionario
+                print "Enviando ", "SIP/2.0 200 OK" + " REGISTER" + '\r\n\r\n'
+                self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
+                self.register2file()
+                for direccion in diccionario.keys():
+                    if timenow >= diccionario[direccion][1]:
+                        del diccionario[direccion]
+                        print direccion + " ha sido borrado" + '\r\n'
+                        print diccionario
+                self.register2file()
+
+    def register2file(self):
+        fich = open('registered.txt', 'w')
+        fich.write("User\tIP\tExpires\r\n")
+        for Client in diccionario.keys():
+            IP = diccionario[Client][0]
+            timexp = diccionario[Client][1]
+            time_new = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(timexp))
+            fich.write(Client + '\t' + IP + '\t' + time_new + '\r\n')
+
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
     diccionario = {}
     serv = SocketServer.UDPServer(("", int(sys.argv[1])), SIPRegisterHandler)
-    print "Lanzando servidor UDP de eco..."
+    print "Lanzando servidor UDP de eco..." + '\r\n'
     serv.serve_forever()
