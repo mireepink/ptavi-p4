5dff22aa901b420edf32942eaea0a0986e8c0ef5
mireya elisabet valenzuela carballo <mireya@iota20.aulas.gsyc.es>
2014-10-27 10:35:50 +0100

    ej.6 REGISTER client

--- client.py
+++ client.py
@@ -13,18 +13,21 @@ SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 
 # Contenido que vamos a enviar
-LINE = " ".join(sys.argv[3:])
+REGISTER = sys.argv[4]
+#EXPIRES = sys.argv[5]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
+print '\r\n' + "REGISTER sip:" + REGISTER + " SIP/2.0" + '\r\n\r\n'
+my_socket.send("REGISTER sip:" + REGISTER + " SIP/2.0")
+#print "Expires: " + EXPIRES
+#my_socket.send(EXPIRES + '\r\n\r\n')
 data = my_socket.recv(1024)
 
-print 'Recibido -- ', data
+print  data
 print "Terminando socket..."
 
 # Cerramos todo
