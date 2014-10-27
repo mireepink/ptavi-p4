28b31720f567cf99bfb27a2256f8f847af7f11e5
javier baos mora <jbaos@iota27.aulas.gsyc.es>
2014-10-27 15:39:09 +0100

    servidor modificado ejercicio 8 pep8

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Clase (y programa principal) para un servidor
+Clase (y programa principal) para un servidor SIP
 """
 
 import SocketServer
@@ -9,48 +9,49 @@ import sys
 import time
 
 
-class  SIPRegisterHandler(SocketServer.DatagramRequestHandler):
+class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Clase SIPRegisterHandler
     """
     lista = []
     diccionario = {}
-    
+
     def register2file(self):
         """
         Metodo que rellena el fichero
         """
-        fich = open('registered.txt','w')
+        fich = open('registered.txt', 'w')
         fich.write("User" + "\t" + "IP" + "\t" + "Expires" + "\r\n")
         for direccion in self.diccionario.keys():
-            fich.write(direccion + '\t' + self.diccionario[direccion][0] + '\t')
-            fich.write(time.strftime('%Y-%m-%d %H:%M:%S',\
-                time.gmtime(self.diccionario[direccion][1])) + "\r\n")
+            ip = self.diccionario[direccion][0]
+            fich.write(direccion + '\t' + ip + '\t')
+            estructura = time.gmtime(self.diccionario[direccion][1])
+            fich.write(time.strftime('%Y-%m-%d %H:%M:%S', estructura) + "\r\n")
         fich.close()
 
     def handle(self):
         """
-        Metodo handle
+        Metodo que trata la peticion REGISTER
         """
         while 1:
             line = self.rfile.read()
             entrada = line.split(' ')
+            print line
             if entrada[0] == 'REGISTER':
-                hora = float(time.time())+ float(entrada[5])
-                self.lista = [self.client_address[0],hora] 
+                hora = float(time.time()) + float(entrada[5])
+                self.lista = [self.client_address[0], hora]
                 self.diccionario[entrada[2]] = self.lista
                 hora_actual = float(time.time())
                 for direccion in self.diccionario.keys():
                     if self.diccionario[direccion][1] < hora_actual:
                         del self.diccionario[direccion]
-                if int(entrada[5]) == 0:
-                    del self.diccionario[entrada[2]]
-                print entrada[3] + " 200 " + "OK\r\n\r\n"
+                print "Enviando..." + entrada[3] + " 200 " + "OK\r\n\r\n"
                 self.wfile.write(entrada[3] + " 200 " + "OK\r\n\r\n")
+            else:
+                print "Peticion erronea"
             if not line:
                 break
             self.register2file()
-            print self.diccionario
 
 if __name__ == "__main__":
     """
