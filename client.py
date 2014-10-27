bd8577b889a6dcc41d0c0c7d613125fd39944b34
silvia marin mateos <smarin@iota37.aulas.gsyc.es>
2014-10-27 11:23:24 +0100

    ejercicio8-client

--- client.py
+++ client.py
@@ -7,32 +7,32 @@ Programa cliente que abre un socket a un servidor
 import sys
 import socket
 
+print sys.argv
+
+if len(sys.argv) != 6:
+    sys.exit("Usage: client.py ip puerto register sip_address expires_value")
+
 SERVER = sys.argv[1]
 PORT = sys.argv[2]
 
 # Contenido que vamos a enviar
 LINE = sys.argv[3]
 registro = 'REGISTER sip:' + sys.argv[4] + ' SIP/1.0\r\n'
-EXPIRES = sys.argv[5]
+EXPIRES = 'Expires: ' + sys.argv[5] + '\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, int(PORT)))
 
-if len(sys.argv) < 5:
-	print "Usage: client.py ip puerto register sip_address expires_value"
-else:
-	if (LINE == 'register'):
-		print "Enviando registro: " + registro
-		expires = EXPIRES + "\r\n\r\n"
-		print 'Expires: ' + expires
-	my_socket.send(registro + expires)
-	data = my_socket.recv(1024)
+if (LINE == 'register'):
+    print "Enviando registro: " + registro + EXPIRES
+    my_socket.send(registro + EXPIRES)
+    data = my_socket.recv(1024)
 
-	print 'Recibido -- ', data
-	print "Terminando socket..."
+print 'Recibido -- ', data
+print "Terminando socket..."
 
-	# Cerramos todo
-	my_socket.close()
-	print "Fin."
+# Cerramos todo
+my_socket.close()
+print "Fin."
