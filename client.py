f9768c2a877e347cbb40c4f98016c9833db9db55
javier baos mora <jbaos@iota27.aulas.gsyc.es>
2014-10-27 15:39:22 +0100

    cliente modificado ejercicio 8 pep8

--- client.py
+++ client.py
@@ -7,26 +7,27 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 
-Entrada = sys.argv
+entrada = sys.argv
 
-if len(Entrada) != 6:
+if len(entrada) != 6:
     sys.exit("Usage: client.py ip puerto register sip_address expires_value")
 
-IP = Entrada[1]
-PORT = int(Entrada[2])
-PETICION = Entrada[3].upper()
+IP = entrada[1]
+PORT = int(entrada[2])
+PETICION = entrada[3].upper()
 
-DIRECCION = Entrada[4]
-TIEMPO = Entrada[5]
+DIRECCION = entrada[4]
+TIEMPO = entrada[5]
 
+LINEA = PETICION + " sip:" + DIRECCION + ' SIP/2.0 ' + '\r\n'
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((IP, PORT))
 
 print PETICION + " sip:" + DIRECCION
 print "Expires: " + TIEMPO
-my_socket.send(PETICION + " sip: " + DIRECCION + ' SIP/2.0 ' + '\r\n' + "Expires: " + TIEMPO + '\r\n\r\n')
-data = my_socket.recv(1024)    
+my_socket.send(LINEA + "Expires: " + TIEMPO + '\r\n\r\n')
+data = my_socket.recv(1024)
 print 'Recibido -- ', data
 print "Terminando socket..."
 
